87dceb664f53ac3c54bb30e764dbb145e593baaf
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-06-22 11:08:45 +0700

    feat: create page components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <script setup>
+import { ref } from 'vue';
 import TheHeader from './components/TheHeader.vue'
 import TheNav from './components/TheNav.vue'
+import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from './constants';
+import TheActivities from './pages/TheActivities.vue';
+import TheProgress from './pages/TheProgress.vue';
+import TheTimeLine from './pages/TheTimeLine.vue';
+
+// определение конкретной страницы для подсветки пункта меню
+const currentPage = ref(normalizePageHash());
+
+// определяем валидное значение хеша на основании названия страниц
+// которые получаем из ключей объекта NavItem и уже это валидное значение хеша
+// присваиваем реактивной переменной currentPage
+function normalizePageHash() {
+  const hash = window.location.hash.slice(1)
+
+  if([PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS].includes(hash)) {
+    return hash
+  }
+  window.location.hash = PAGE_TIMELINE
+
+  return PAGE_TIMELINE
+}
 </script>
 
 <template>
   <TheHeader />
 
-  <main class="flex flex-grow flex-col">.
+  <main class="flex flex-grow flex-col">
+    <TheTimeLine v-show="currentPage === PAGE_TIMELINE" />
+    <TheActivities v-show="currentPage === PAGE_ACTIVITIES" />
+    <TheProgress v-show="currentPage === PAGE_PROGRESS" />
   </main>
 
-  <TheNav />
+  <TheNav :current-page="currentPage" @navigate="currentPage = $event" />
 </template>
--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,31 +1,19 @@
 <script setup>
-import { ref } from 'vue';
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline';
 import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '../constants';
 import NavItem from './NavItem.vue';
+
+// принимаем кастомные атрибуты
+defineProps(['currentPage'])
+
+// отправка кастомных событий в родительский компонент
+const emit = defineEmits(['navigate'])
 
 const navItems = {
   [PAGE_TIMELINE]: ClockIcon,
   [PAGE_ACTIVITIES]: ListBulletIcon,
   [PAGE_PROGRESS]: ChartBarIcon
 };
-
-// определение конкретной страницы для подсветки пункта меню
-const currentPage = ref(normalizePageHash());
-
-// определяем валидное значение хеша на основании названия страниц
-// которые получаем из ключей объекта NavItem и уже это валидное значение хеша
-// присваиваем реактивной переменной currentPage
-function normalizePageHash() {
-  const hash = window.location.hash.slice(1)
-
-  if(Object.keys(navItems).includes(hash)) {
-    return hash
-  }
-  window.location.hash = PAGE_TIMELINE
-
-  return PAGE_TIMELINE
-}
 </script>
 
 <template>
@@ -36,7 +24,7 @@ function normalizePageHash() {
         :key="page"
         :href="`#${page}`"
         :class="{ 'bg-gray-200 pointer-events-none': page === currentPage }"
-        @click="currentPage = page"
+        @click="emit('navigate', page)"
       >
         <component :is="icon" class="h-6 w-6" /> {{ page }}
       </NavItem>
