c9b7456b2ada50bcbf24cfd988c6dfb978541def
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-06-23 12:05:39 +0700

    feat: create generating timeline data

--- src/functions.js
+++ src/functions.js
@@ -1,4 +1,9 @@
-import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from './constants';
+import {
+  PAGE_TIMELINE,
+  PAGE_ACTIVITIES,
+  PAGE_PROGRESS,
+  HOURS_IN_DAY,
+} from './constants';
 
 export function normalizePageHash() {
   const hash = window.location.hash.slice(1)
@@ -9,4 +14,15 @@ export function normalizePageHash() {
   window.location.hash = PAGE_TIMELINE
 
   return PAGE_TIMELINE
+};
+
+// генерация данных временной шкалы
+export function generateTimeLineItems() {
+  const timelineItems = []
+
+  for (let hour = 0; hour < HOURS_IN_DAY; hour++) {
+    timelineItems.push({ hour })
+  }
+
+  return timelineItems
 };
