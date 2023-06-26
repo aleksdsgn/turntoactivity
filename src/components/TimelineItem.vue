c6845ebe9eb0e82738dd61207b8c6e63d828e5a8
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-06-26 16:57:55 +0700

    feat: add timeline hour component

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -2,8 +2,9 @@
 import { ref } from 'vue';
 import { isTimelineItemValid } from '../validators';
 import BaseSelect from './BaseSelect.vue';
+import TimelineHour from './TimelineHour.vue';
 
-const props = defineProps({
+defineProps({
   timelineItem: {
     required: true,
     type: Object,
@@ -11,12 +12,6 @@ const props = defineProps({
   }
 });
 
-const hourLinkClasses = [
-  'absolute -top-4 left-1/2 -translate-x-1/2 rounded px-2 font-mono text-lg',
-  props.timelineItem.hour === new Date().getHours()
-    ? 'bg-purple-900 font-black text-white'
-    : 'bg-gray-100 text-gray-500'
-];
 
 // каждой активности свой идентификатор
 const options = [
@@ -32,12 +27,7 @@ const selectedActivityId = ref(1);
 
 <template>
   <li class="relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4">
-    <a
-      href="#"
-      :class="hourLinkClasses"
-    >
-      {{ timelineItem.hour }}:00
-    </a>
+    <TimelineHour :hour="timelineItem.hour" />
     <!-- передаем опции в качестве внешнего свойства компоненту BaseSelect -->
     <BaseSelect
       :selected="selectedActivityId"
