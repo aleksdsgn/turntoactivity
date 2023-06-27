5266ceb473bc044761fea838e3663712774e3a55
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-06-27 16:26:58 +0700

    feat: add create activity

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,10 @@ function goTo(page) {
   currentPage.value = page
 };
 
+function createActivity(activity) {
+  activities.value.push(activity)
+};
+
 function deleteActivity(activity) {
   activities.value.splice(activities.value.indexOf(activity), 1)
 };
@@ -43,6 +47,7 @@ function deleteActivity(activity) {
     <TheActivities
       v-show="currentPage === PAGE_ACTIVITIES"
       :activities="activities"
+      @create-activity="createActivity"
       @delete-activity="deleteActivity"
     />
     <TheProgress v-show="currentPage === PAGE_PROGRESS" />
