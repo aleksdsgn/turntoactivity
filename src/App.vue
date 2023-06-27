efe467e1d68dfd5b06c1d51e629b5a8567b293c6
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-06-27 14:54:51 +0700

    feat: add delete activity

--- src/App.vue
+++ src/App.vue
@@ -18,12 +18,16 @@ const currentPage = ref(normalizePageHash());
 // данные временной шкалы
 const timelineItems = generateTimeLineItems();
 
-const activities = ['Coding', 'Reading', 'Training'];
+const activities = ref(['Coding', 'Reading', 'Training']);
 
-const activitySelectOptions = generateActivitySelectOptions(activities);
+const activitySelectOptions = generateActivitySelectOptions(activities.value);
 
 function goTo(page) {
   currentPage.value = page
+};
+
+function deleteActivity(activity) {
+  activities.value.splice(activities.value.indexOf(activity), 1)
 };
 </script>
 
@@ -36,7 +40,11 @@ function goTo(page) {
       :timeline-items="timelineItems"
       :activity-select-options="activitySelectOptions"
     />
-    <TheActivities v-show="currentPage === PAGE_ACTIVITIES" :activities="activities" />
+    <TheActivities
+      v-show="currentPage === PAGE_ACTIVITIES"
+      :activities="activities"
+      @delete-activity="deleteActivity"
+    />
     <TheProgress v-show="currentPage === PAGE_PROGRESS" />
   </main>
 
