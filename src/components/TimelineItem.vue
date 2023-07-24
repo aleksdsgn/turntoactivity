5bac00b765b7ef601906f76a9682f13b7f77073a
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-07-24 16:48:09 +0700

    feat: add disabled stopwatch buttons

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -51,6 +51,6 @@ function findActivityById(id) {
       :options="activitySelectOptions"
       @select="selectActivity"
     />
-    <TimelineStopwatch :seconds="timelineItem.activitySeconds" />
+    <TimelineStopwatch :seconds="timelineItem.activitySeconds" :hour="timelineItem.hour" />
   </li>
 </template>
