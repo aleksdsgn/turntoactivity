7c513160f941545c4bc6d1e887dee1423f94565a
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-08-10 10:41:30 +0700

    refator: inject activities and activity  selrct options into TimelineItem component

--- src/App.vue
+++ src/App.vue
@@ -65,7 +65,9 @@ function setActivitySecondsToComplete(activity, secondsToComplete) {
 };
 
 provide('updateTimelineItemActivitySeconds', updateTimelineItemActivitySeconds);
+provide('activitySelectOptions', activitySelectOptions.value);
 provide('timelineItems', timelineItems.value);
+provide('activities', activities.value);
 </script>
 
 <template>
@@ -75,8 +77,6 @@ provide('timelineItems', timelineItems.value);
     <TheTimeLine
       v-show="currentPage === PAGE_TIMELINE"
       :timeline-items="timelineItems"
-      :activities="activities"
-      :activity-select-options="activitySelectOptions"
       :current-page="currentPage"
       ref="timeline"
       @set-timeline-item-activity="setTimelineItemActivity"
